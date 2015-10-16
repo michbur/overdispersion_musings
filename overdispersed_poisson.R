e48985bf07d83eecd83ad1f4e05ffe9da6ee7e06
michbur <michalburdukiewicz@gmail.com>
2015-10-17 00:59:14 +0200

    calculated bias for overdispersed data

--- overdispersed_poisson.R
+++ overdispersed_poisson.R
@@ -9,6 +9,7 @@ dpois(5, 2)
 
 library(ggplot2)
 library(reshape2)
+library(dplyr)
 
 compare_dens <- function(lambdas = c(0.1, 0.5, 1, 2), quantiles = 0L:10, ps = 0:4/4) {
   melt(do.call(rbind, lapply(lambdas, function(single_lambda) {
@@ -33,12 +34,33 @@ ggplot(mdf_dens, aes(x = quant, y = value, fill = p)) +
 #probability function from Wang 2012
 #p overdispersed/underdispersed Poisson
 #if p > 1, underdispersion
-doupois <- function(x, p, lambda) {
-  if(p <= (1 - exp(-lambda))^-1)
-    stop("Probability distribution not longer valid")
-  if(x == 0) {
-    (1 - p) + p*exp(-lambda)
-  } else {
-    p * ((lambda^x) * exp(-lambda))/(factorial(x))
-  }
+doupois <- function(x, lambda, p) {
+  if(p > (1 - exp(-lambda))^-1)
+    stop("Probability distribution no longer valid.")
+  sapply(x, function(ith_x) {
+    if(ith_x == 0) {
+      (1 - p) + p*exp(-lambda)
+    } else {
+      p * ((lambda^ith_x) * exp(-lambda))/(factorial(ith_x))
+    }
+  })
 }
+# Example:
+#doupois(5, 1, 1)
+#dpois(5, 1)
+
+dens <- sapply(5L:10/10, function(single_p)
+  doupois(0L:7, 1, single_p))
+mdens <- melt(dens, varnames = c("k", "p"))
+mdens[["p"]] <- factor(mdens[["p"]], labels = 5L:10/10)
+mdens[["k"]] <- factor(mdens[["k"]], labels = 0L:7)
+
+library(ggplot2)
+
+ggplot(mdens, aes(x = k, fill = p, y = value)) +
+  geom_bar(stat = "identity", position = "dodge")
+
+mutate(mdens, expected = as.numeric(as.character(k)) * value) %>% 
+  group_by(p) %>%
+  summarize(est_mean = (sum(expected))) %>%
+  mutate(bias = 1 - est_mean)
